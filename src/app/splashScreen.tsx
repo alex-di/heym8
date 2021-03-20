0781e7cbc239b84fbeb95f428aac5d83c8a9c8d7
Diakov, Aleksei <a.diakov@fxpro.com>
2021-03-20 16:41:52 +0200

    Mobile check

--- src/app/splashScreen.tsx
+++ src/app/splashScreen.tsx
@@ -9,10 +9,14 @@ export default () => {
         <div className=" d-flex flex-column justify-content-center ">
             
   <h1>Hey mate!</h1>
+  <p></p>
   <p>
     This is the lightweight app that allows you to have a video chat with your "mate" without a middleman.<br/>
     It stores nothing on the server and has no tracking or analytics.
   </p>
+  <p>To connect with your friends just share this link and wait for them <br/>to enjoy private chatting limited only by your connection capabilities.</p>
+  
+    <ClipboardText></ClipboardText>
   <p>
     Messages and streams are transferred directly between peers using super secure WebRTC technology.
   </p>
@@ -22,13 +26,11 @@ export default () => {
   <p>
     <Button variant="primary" href="https://github.com/alex-di/heym8" target="_blank">Learn more</Button>
   </p>
-  <p>To connect with your friends just share this link and wait for them <br/>to enjoy private chatting limited only by your connection capabilities.</p>
-  
-    <ClipboardText></ClipboardText>
     <p></p>
     <p></p>
     <p></p>
   <h3 className="text-right"><i>Cheers, Alex Di.</i></h3>
+  <p></p>
   <p><i>P.S: I'd recomment using Chrome for this</i></p>
     </div>
 
